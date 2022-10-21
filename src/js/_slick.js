0d222a5a0a28b677f48d91f85a06ba59d790a497
Yevhenii <boiko.yevgenii@gmail.com>
2022-10-21 15:08:23 +0300

    adds new pagination

---
+++ src/js/_slick.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+  $('.review-slider').slick({
+    arrows: false,
+    dots: true,
+    autoplay: false,
+    pauseOnFocus: true,
+    pauseOnHover: true,
+    pauseOnDotsHover: true,
+    fade: true,
+  });
+});
